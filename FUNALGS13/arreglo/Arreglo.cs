716cf6ada6a805ff2bebfbf2768dfdb18f694053
Pierina01011ch <pierina21op15A@gmail.com>
2024-11-13 22:06:57 -0500

    Primer commit

---
+++ FUNALGS13/arreglo/Arreglo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglo
+{
+    internal class Arreglo
+    {
+        private int[] arreglo;
+
+        public Arreglo(int n)
+        {
+            arreglo = new int[n];
+        }
+
+        public void LlenarArreglo()
+        {
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                Console.Write($"Elemento {i + 1}: ");
+                arreglo[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void MostrarArreglo()
+        {
+            foreach (int valor in arreglo)
+            {
+                Console.Write(valor + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void EliminarElemento(int posicion)
+        {
+            if (posicion >= 0 && posicion < arreglo.Length)
+            {
+                for (int i = posicion; i < arreglo.Length - 1; i++)
+                {
+                    arreglo[i] = arreglo[i + 1]; 
+                }
+
+                Array.Resize(ref arreglo, arreglo.Length - 1);
+            }
+            else
+            {
+                Console.WriteLine("La posición ingresada es inválida.");
+            }
+        }
+
+    }
+
+}
+
---
+++ FUNALGS13/arreglo/Program.cs
@@ -0,0 +1,27 @@
+﻿using arreglo;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el tamaño del arreglo: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Arreglo arreglo = new Arreglo(n);
+
+        Console.WriteLine("Ingrese los valores del arreglo:");
+        arreglo.LlenarArreglo();
+
+        Console.WriteLine("\nArreglo original:");
+        arreglo.MostrarArreglo();
+
+        Console.Write("\nFavor de ingresar la posición del elemento a eliminar (empezando desde 0): ");
+        int posicion = int.Parse(Console.ReadLine());
+
+        arreglo.EliminarElemento(posicion);
+
+        Console.WriteLine("\nArreglo después de la eliminación:");
+        arreglo.MostrarArreglo();
+    }
+}
+
